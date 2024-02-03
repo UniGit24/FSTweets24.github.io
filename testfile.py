00094cf315e545609181a115fac7ea6ef7a68c57
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2024-02-03 12:24:27 +0000

    file updates

--- testfile.py
+++ testfile.py
@@ -36,19 +36,16 @@ def detector(topic,tweet):
     
 def main(country, topic, file, date):
     print(country, topic, file, date)
-    df = pd.read_csv('August21Data.csv')
-   # df = pd.read_csv('Book2.csv')
-    #df = pd.read_csv(file)
+    #df = pd.read_csv('August21Data.csv')
+    #df = pd.read_csv('Book2.csv')
+    df = pd.read_csv(file)
     df2 = df[df['searched_hashtag_country'] == country]
-    df3 = df2[["id","searched_hashtag_country","tweet_text","latitude","longitude"]]
-    hashcountry = df3["searched_hashtag_country"]
+    df3 = df2[["searched_hashtag_country","tweet_text","latitude","longitude"]]
     arr = df3["tweet_text"]
     lat = df3["latitude"]
     lon = df3["longitude"]
-    id = df3["id"]
-    #hashcountry.index(country)
-    tempi = hashcountry.index("Canada")
-    print(tempi)
+    #id = df3["id"]
+    print(arr)
     new_arr = []
     new_lat = []
     new_lon = []
@@ -67,23 +64,25 @@ def main(country, topic, file, date):
         print(score)
         index_list = []
         for y in score:
-            if y>70:
+            if y>80:
             #    print()
             #    d["text"].append(x)
             #    latitude = lat[index]
             #    print(latitude)
             #    d["lat"].append(lat[index])
             #    d["lon"].append(lon[index])
-                print(index)
-                temp_index = id[index] - 1
-                #print(temp_index)
-                #print(arr)
+                temp_index = index - 1
+                print(temp_index)
+                print(arr)
                 #print(id)
-                new_arr.append(arr[temp_index])
-                new_lat.append(lat[temp_index])
-                new_lon.append(lon[temp_index])
-                temp_score = y
-                new_score.append(((temp_score.item()-65)*5))
+                try:
+                    new_arr.append(arr[temp_index])
+                    new_lat.append(lat[temp_index])
+                    new_lon.append(lon[temp_index])
+                    temp_score = y
+                    new_score.append(((temp_score.item()-75)*5))
+                except:
+                    index = index + 1
     #for index, x in enumerate(arr, start=1): 
         #for y in index_list:
             #if y == index:
@@ -151,7 +150,7 @@ def main(country, topic, file, date):
             geo_scope='north america'
         )
     fig.show()
-    filename = topic +".html"
+    filename = topic + date +".html"
     fig.write_html(filename)
     #fig.write_html("austweet1.html")
     #fdf = pd.DataFrame(data=d)
@@ -159,20 +158,26 @@ def main(country, topic, file, date):
     #fig.show()
 
 def initialiser (country):
-    Australia_topics = ["Victoria to enter sixth lockdown","26 June – Greater Sydney, Wollongong, Blue Mountains and the Central Coast are placed into lockdown as the Delta variant of COVID-19 spreads.", " 11 July – Australia records its first death from the COVID-19 pandemic for 2021,as Sydney records 77 cases of community transmission.", "16 July – Melbourne enters snap lockdown with 18 cases of COVID-19.", "21 August – New South Wales records the highest daily COVID-19 case numbers in Australia thus far, recording 825 new cases of COVID-19.","25 August – New South Wales records 1,029 new cases of COVID-19 in 24 hours becoming the first state in Australia to surpass the 1,000 daily case milestone."]
+    Australia_topics = ["August 9 - Victoria to enter 6th lockdown", "26 June – Greater Sydney, Wollongong, Blue Mountains and the Central Coast are placed into lockdown as the Delta variant of COVID-19 spreads.", " 11 July – Australia records its first death from the COVID-19 pandemic for 2021,as Sydney records 77 cases of community transmission.", "16 July – Melbourne enters snap lockdown with 18 cases of COVID-19.", "21 August – New South Wales records the highest daily COVID-19 case numbers in Australia thus far, recording 825 new cases of COVID-19.","25 August – New South Wales records 1,029 new cases of COVID-19 in 24 hours becoming the first state in Australia to surpass the 1,000 daily case milestone."]
     USA_topics = ["June 1 - SARS-CoV-2 Delta variant becomes the dominant strain of COVID-19 in the United States", "June 1 - COVID-19 vaccines – Moderna seeks full approval from the FDA for the Moderna COVID-19 vaccine", "June 5 – Aftermath of the January 6 United States Capitol attack – The Department of Justice says that over 465 people have been arrested since the January 6 attack. It is also seeking information on 250 other suspects.", "July 20 - Tom Barrack, founder of Colony Capital and an advisor of Donald Trump, is indicted for making false statements to the FBI and being an unregistered agent for the United Arab Emirates.", "August 2 - COVID-19 vaccination: Over 70% of adults are reported to have received at least one dose of a COVID-19 vaccine.", "August 10 - New York Governor Andrew Cuomo announces he will resign effective August 24 after an inquiry found he sexually harassed multiple women.", "August 29 - Hurricane Ida makes landfall at 11:55am CDT near Port Fourchon, Louisiana, on the 16th anniversary of Hurricane Katrina."]
     Canada_topics = ["June 21 – The Government of Canada announces the first phase to easing the COVID-19 border measures for travellers, thus lifting quarantine requirements for fully immunised travellers starting on July 5", "June 30 - Dozens of people have died amid an unprecedented heatwave that has smashed temperature records.", "July 20 – British Columbia declares a state of emergency in response to the 2021 British Columbia wildfires.", "August 2 -  SARS-CoV-2 Delta variant becomes the pre-dominant strain of COVID-19 in Canada."]
     if (country == 'Australia'): 
         for topic in Australia_topics:
             #topic = "Victoria to enter sixth lockdown"
-            file = 'August21Data.csv'
-            date = 'August 21'
-            main(country, topic, file, date)
+            #file = 'August21Data.csv'
+            #date = 'August 21'
+            #main(country, topic, file, date)
             file = '2021_July_twitter_trending_data.csv'
             date = 'July 21'
             main(country, topic, file, date)
             file = '2021_June_twitter_trending_data.csv'
             date = 'June 21'
+            main(country, topic, file, date)
+            file = '2021_September_twitter_trending_data.csv'
+            date = 'September 21'
+            main(country, topic, file, date)
+            file = '2021_October_twitter_trending_data.csv'
+            date = 'October 21'
             main(country, topic, file, date)
 
     if (country == 'Canada'): 
