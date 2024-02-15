926e16f3faaf2b73329b1fa9c33ee6d807412120
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2024-02-15 14:53:49 +0000

    canada updates

--- MAIN.py
+++ MAIN.py
@@ -189,6 +189,31 @@ def main(country, topic, file, date):
             title = ("Number of tweets:" + str(temp_no_scores) + " Date: " + str(date) + " " + topic + "Tweets found: {}".format(tweetsfound)),
             geo_scope='north america'
         )
+        if (date == 'June 21'):
+                june_no
+                june_found
+                june_no = temp_no_scores
+                june_found = tweetsfound
+        if (date == 'July 21'):
+                july_no
+                july_found
+                july_no = temp_no_scores
+                july_found = tweetsfound
+        if (date == 'August 21'):
+                august_no
+                august_found
+                august_no = temp_no_scores
+                august_found = tweetsfound
+        if (date == 'September 21'):
+                september_no
+                september_found
+                september_no = temp_no_scores
+                september_found = tweetsfound
+        if (date == 'October 21'):
+                october_no
+                october_found
+                october_no = temp_no_scores
+                october_found = tweetsfound
     fig.show()
     filename = topic + date +".html"
     fig.write_html(filename)
@@ -201,6 +226,9 @@ def initialiser (country):
     Australia_topics = []
     Australia_dates = []
     parseAustraliaTopics = []
+    Canada_topics = []
+    Canada_dates = []
+    parseCanadaTopics = []
     # Opening JSON file
     f = open('bloomberg_quint_news (1).json',)
     # returns JSON object as
@@ -219,7 +247,12 @@ def initialiser (country):
         if "US" in i['title']:
             print(i['title'])
         if "Canada" in i['title']:
-            print(i['title'])
+            if "June 2021" or "July 2021" or "August 2021" or "Septemebr 2021" or "October 2021" in i['date_created']:
+                Canada_topics.append(i['title'])
+                Canada_dates.append(i['date_created'])
+                print(i['title'])
+            else:
+                print(" ")
     # Closing file
     f.close()
     for index, x in enumerate(Australia_topics, start=0): 
@@ -227,7 +260,11 @@ def initialiser (country):
         print(y)
         parseAustraliaTopics.append(y)
     USA_topics = ["June 1 - SARS-CoV-2 Delta variant becomes the dominant strain of COVID-19 in the United States", "June 1 - COVID-19 vaccines – Moderna seeks full approval from the FDA for the Moderna COVID-19 vaccine", "June 5 – Aftermath of the January 6 United States Capitol attack – The Department of Justice says that over 465 people have been arrested since the January 6 attack. It is also seeking information on 250 other suspects.", "July 20 - Tom Barrack, founder of Colony Capital and an advisor of Donald Trump, is indicted for making false statements to the FBI and being an unregistered agent for the United Arab Emirates.", "August 2 - COVID-19 vaccination: Over 70% of adults are reported to have received at least one dose of a COVID-19 vaccine.", "August 10 - New York Governor Andrew Cuomo announces he will resign effective August 24 after an inquiry found he sexually harassed multiple women.", "August 29 - Hurricane Ida makes landfall at 11:55am CDT near Port Fourchon, Louisiana, on the 16th anniversary of Hurricane Katrina."]
-    Canada_topics = ["June 21 – The Government of Canada announces the first phase to easing the COVID-19 border measures for travellers, thus lifting quarantine requirements for fully immunised travellers starting on July 5", "June 30 - Dozens of people have died amid an unprecedented heatwave that has smashed temperature records.", "July 20 – British Columbia declares a state of emergency in response to the 2021 British Columbia wildfires.", "August 2 -  SARS-CoV-2 Delta variant becomes the pre-dominant strain of COVID-19 in Canada."]
+    for index, x in enumerate(Canada_topics, start=0): 
+        y = str(x + str(Canada_dates[index]))
+        print(y)
+        parseCanadaTopics.append(y)
+   #Canada_topics = ["June 21 – The Government of Canada announces the first phase to easing the COVID-19 border measures for travellers, thus lifting quarantine requirements for fully immunised travellers starting on July 5", "June 30 - Dozens of people have died amid an unprecedented heatwave that has smashed temperature records.", "July 20 – British Columbia declares a state of emergency in response to the 2021 British Columbia wildfires.", "August 2 -  SARS-CoV-2 Delta variant becomes the pre-dominant strain of COVID-19 in Canada."]
     if (country == 'Australia'): 
         for topic in parseAustraliaTopics:
             #topic = "Victoria to enter sixth lockdown"
@@ -271,23 +308,45 @@ def initialiser (country):
 
 
     if (country == 'Canada'): 
-        for topic in Canada_topics:
+        for topic in parseCanadaTopics:
             #topic = "Victoria to enter sixth lockdown"
-            file = 'August21Data.csv'
+            file = 'August21Datacan.csv'
             date = 'August 21'
             main(country, topic, file, date)
-            file = '2021_July_twitter_trending_data.csv'
+            file = '2021_July_twitter_trending_datacan.csv'
             date = 'July 21'
             main(country, topic, file, date)
-            file = '2021_June_twitter_trending_data.csv'
+            file = '2021_June_twitter_trending_datacan.csv'
             date = 'June 21'
             main(country, topic, file, date)
-            file = '2021_September_twitter_trending_data.csv'
+            file = '2021_September_twitter_trending_datacan.csv'
             date = 'September 21'
             main(country, topic, file, date)
-            file = '2021_October_twitter_trending_data.csv'
+            file = '2021_October_twitter_trending_datacan.csv'
             date = 'October 21'
             main(country, topic, file, date)
+
+            # List that we want to add as a new row
+            june_percent = ((june_found/june_no)*100)
+            july_percent = ((july_found/july_no)*100)
+            august_percent = ((august_found/august_no)*100)
+            september_percent = ((september_found/september_no)*100)
+            october_percent = ((october_found/october_no)*100)
+
+            List = [[country,topic,june_no,june_found,june_percent,july_no,july_found,july_percent,august_no,august_found,august_percent,september_no,september_found,september_percent,october_no,october_found,october_percent]]
+            print(List)
+            # Open our existing CSV file in append mode
+            # Create a file object for this file
+            with open('FINALCAN.csv', 'a', newline = '') as f_object:
+                # Pass this file object to csv.writer()
+                # and get a writer object
+                writer_object = csv.writer(f_object)
+        
+                # Pass the list as an argument into the writerow()
+                writer_object.writerows(List)
+        
+                # Close the file object
+        f_object.close()  
 
     if (country == 'United States'): 
         for topic in USA_topics:
