dc7b02bd1cb964754b0be73a2a67a355fb053a39
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2024-05-03 16:05:11 +0100

    file delete

--- databasepostgres.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE DataAnalysis (
-    Country varchar(8000),
-    Topic varchar(8000),
-    Frequency_june int,
-    Frequency_july int,
-    Frequency_august int,
-    latitude float,
-    longitude float
-);
--- randtestfile.py
+++
@@ -1,6 +0,0 @@
-import csv
-List = ["hey","hi","yo","hi"]
-with open('FINALAUS1.csv', 'a', newline = '') as f_object:
-    writer_object = csv.writer(f_object)
-    writer_object.writerow(List)
-f_object.close() 
